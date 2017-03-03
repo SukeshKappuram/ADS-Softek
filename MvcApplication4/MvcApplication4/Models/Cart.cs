cd19700b47fa3ad29e1f6067ceaee06f1518399c
Sukeshk6 <iamsukeshk@gmail.com>
2017-03-03 12:26:36 +0530

    Build version 2017.03.03.12.26

--- MvcApplication4/MvcApplication4/Models/Cart.cs
+++ MvcApplication4/MvcApplication4/Models/Cart.cs
@@ -5,9 +5,10 @@ using System.Web;
 
 namespace MvcApplication4.Models
 {
-    public class Role
+    public class Cart
     {
-        public string roleName { get; set; }
+        public int Id { get; set; }
         public int userId { get; set; }
+        public int productId {get; set;}
     }
 }
